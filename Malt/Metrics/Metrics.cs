1de08e6727db95d405cf7b30184dbd4cb74d47d7
ytakashina <takashina2051@gmail.com>
2017-03-23 19:49:24 +0900

    Add Metrics.cs

---
+++ Malt/Metrics/Metrics.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Malt.Metrics
+{
+    public static class Metrics
+    {
+        public static Func<(double d, int n), (double d, int n), double> Shortest = (c1, c2) => c1.d < c2.d ? c1.d : c2.d;
+        public static Func<(double d, int n), (double d, int n), double> Longest = (c1, c2) => c1.d > c2.d ? c1.d : c2.d;
+        public static Func<(double d, int n), (double d, int n), double> GroupAverage = (c1, c2) => (double) c1.n / (c1.n + c2.n) * c1.d + (double) c2.n / (c1.n + c2.n) * c2.d;
+    }
+}
