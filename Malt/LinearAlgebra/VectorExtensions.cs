17ef53809c925951f86410cf719133f16a2c2abf
ytakashina <takashina2051@gmail.com>
2017-03-23 20:14:28 +0900

    Add IEnumerable<T>.Print

--- Malt/LinearAlgebra/VectorExtensions.cs
+++ Malt/LinearAlgebra/VectorExtensions.cs
@@ -1,10 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Malt.LinearAlgebra
 {
     public static class VectorExtensions
     {
+        public static void Print<T>(this IEnumerable<T> enumerable)
+        {
+            var array = enumerable as T[] ?? enumerable.ToArray();
+            Console.Write("[");
+            for (var i = 0; i < array.Length; i++)
+            {
+                if (array[i] is IEnumerable<object> inner) inner.Print();
+                else Console.Write(array[i] + (i < array.Length - 1 ? ", " : ""));
+            }
+            Console.WriteLine("]");
+        }
+
         public static double[] Normalize(this IEnumerable<int> enumerable)
         {
             var array = enumerable as int[] ?? enumerable.ToArray();
