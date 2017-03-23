5fd80cc863858be1604e0c500cbb828f2c9bc1d4
ytakashina <takashina2051@gmail.com>
2017-03-23 19:51:29 +0900

    fix wrong namespace

--- Malt/LinearAlgebra/Vector.cs
+++ Malt/LinearAlgebra/Vector.cs
@@ -1,45 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using System.Text;
 
-namespace Malt
+namespace Malt.LinearAlgebra
 {
-    namespace LinearArgebra
+    public static class Vector
     {
-        public static class Vector
+        public static double[] Ones(int n)
         {
-            public static double[] Ones(int n)
-            {
-                var vector = new double[n];
-                for (var i = 0; i < n; i++) vector[i] = 1;
-                return vector;
-            }
+            var vector = new double[n];
+            for (var i = 0; i < n; i++) vector[i] = 1;
+            return vector;
+        }
 
-            public static double[] Zeros(int n)
-            {
-                return new double[n];
-            }
+        public static double[] Zeros(int n)
+        {
+            return new double[n];
+        }
 
-            public static double[] Random(int n, double mean = 0, double amplitude = 1)
-            {
-                var rand = new Random();
-                var vector = new double[n];
-                for (var i = 0; i < n; i++) vector[i] = amplitude * (rand.NextDouble() - 0.5) * 2 + mean;
-                return vector;
-            }
+        public static double[] Random(int n, double mean = 0, double amplitude = 1)
+        {
+            var rand = new Random();
+            var vector = new double[n];
+            for (var i = 0; i < n; i++) vector[i] = amplitude * (rand.NextDouble() - 0.5) * 2 + mean;
+            return vector;
+        }
 
-            public static double[] Gauss(int n, double mean = 0, double deviation = 1)
-            {
-                throw new NotImplementedException();
-            }
+        public static double[] Gauss(int n, double mean = 0, double deviation = 1)
+        {
+            throw new NotImplementedException();
+        }
 
-            public static int[] OneHot(int n, int k)
-            {
-                var vector = new int[n];
-                vector[k] = 1;
-                return vector;
-            }
+        public static int[] OneHot(int n, int k)
+        {
+            var vector = new int[n];
+            vector[k] = 1;
+            return vector;
         }
     }
 }
--- Malt/LinearAlgebra/VectorExtensions.cs
+++ Malt/LinearAlgebra/VectorExtensions.cs
@@ -1,27 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
-namespace Malt
+namespace Malt.LinearAlgebra
 {
-    namespace LinearArgebra
+    public static class VectorExtensions
     {
-        public static class VectorExtensions
+        public static double[] Normalize(this IEnumerable<int> enumerable)
         {
-            public static double[] Normalize(this IEnumerable<int> enumerable)
-            {
-                var array = enumerable as int[] ?? enumerable.ToArray();
-                var sum = array.Sum();
-                return array.Select(v => (double) v / sum).ToArray();
-            }
+            var array = enumerable as int[] ?? enumerable.ToArray();
+            var sum = array.Sum();
+            return array.Select(v => (double) v / sum).ToArray();
+        }
 
-            public static double[] Normalize(this IEnumerable<double> enumerable)
-            {
-                var array = enumerable as double[] ?? enumerable.ToArray();
-                var sum = array.Sum();
-                return array.Select(v => v / sum).ToArray();
-            }
+        public static double[] Normalize(this IEnumerable<double> enumerable)
+        {
+            var array = enumerable as double[] ?? enumerable.ToArray();
+            var sum = array.Sum();
+            return array.Select(v => v / sum).ToArray();
         }
     }
 }
