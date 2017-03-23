f01663df2bf4307d1c3bcd00e84e341ec1e7686c
ytakashina <takashina2051@gmail.com>
2017-03-23 21:41:28 +0900

    Add: unit test

---
+++ MaltTest/MatrixTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Malt.LinearAlgebra;
+using Xunit;
+
+namespace MaltTest
+{
+    public class MatrixTest
+    {
+        [Fact]
+        public void ZerosTest() {}
+
+        [Fact]
+        public void OnesTest() {}
+
+        [Fact]
+        public void UniformTest()
+        {
+            var matrix = Matrix.Uniform(10, 1.0);
+            for (var i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (var j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Assert.Equal(1.0, matrix[i, j]);
+                }
+            }
+        }
+    }
+}
