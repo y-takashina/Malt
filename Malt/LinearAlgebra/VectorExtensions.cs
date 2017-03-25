515d38ae216e246c88abf15f04663043400e56a9
ytakashina <takashina2051@gmail.com>
2017-03-25 17:32:31 +0900

    Add some method to Vector

--- Malt/LinearAlgebra/VectorExtensions.cs
+++ Malt/LinearAlgebra/VectorExtensions.cs
@@ -25,6 +25,11 @@ namespace Malt.LinearAlgebra
             Console.WriteLine();
         }
 
+        public static double Norm(this IEnumerable<double> vector, double order = 2)
+        {
+            return vector.Sum(v => Math.Pow(v, order));
+        }
+
         public static double[] Normalize(this IEnumerable<int> enumerable)
         {
             var array = enumerable as int[] ?? enumerable.ToArray();
@@ -37,6 +42,26 @@ namespace Malt.LinearAlgebra
             var array = enumerable as double[] ?? enumerable.ToArray();
             var sum = array.Sum();
             return array.Select(v => v / sum).ToArray();
+        }
+
+        public static double InnerProduct(this IEnumerable<double> vector1, IEnumerable<double> vector2)
+        {
+            return LinearCombinate(vector1, vector2);
+        }
+
+        public static double[,] OuterProduct(this IEnumerable<double> vector1, IEnumerable<double> vector2)
+        {
+            var array1 = vector1 as double[] ?? vector1.ToArray();
+            var array2 = vector2 as double[] ?? vector2.ToArray();
+            var results = new double[array1.Length, array2.Length];
+            for (var i = 0; i < array1.Length; i++)
+            {
+                for (var j = 0; j < array2.Length; j++)
+                {
+                    results[j, i] = array1[i] * array2[j];
+                }
+            }
+            return results;
         }
 
         public static double LinearCombinate(this IEnumerable<double> variables, IEnumerable<double> weights)
