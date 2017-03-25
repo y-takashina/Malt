a86474b406410a451ec0c1a3ffab45b126f4a0da
ytakashina <takashina2051@gmail.com>
2017-03-25 17:51:16 +0900

    Add Vector.Reshape()

--- Malt/LinearAlgebra/VectorExtensions.cs
+++ Malt/LinearAlgebra/VectorExtensions.cs
@@ -68,5 +68,58 @@ namespace Malt.LinearAlgebra
         {
             return InnerProduct(variables, weights);
         }
+
+        public static T[,] Reshape<T>(this IEnumerable<T> enumerable, (int, int) ints)
+        {
+            var array = enumerable as T[] ?? enumerable.ToArray();
+            var results = new T[ints.Item1, ints.Item2];
+            for (var i = 0; i < ints.Item1; i++)
+            {
+                for (var j = 0; j < ints.Item2; j++)
+                {
+                    results[i, j] = array[i + j];
+                }
+            }
+            return results;
+        }
+
+        public static T[,,] Reshape<T>(this IEnumerable<T> enumerable, (int, int, int) ints)
+        {
+            var array = enumerable as T[] ?? enumerable.ToArray();
+            var results = new T[ints.Item1, ints.Item2, ints.Item3];
+            for (var i = 0; i < ints.Item1; i++)
+            {
+                for (var j = 0; j < ints.Item2; j++)
+                {
+                    for (var k = 0; k < ints.Item2; k++)
+                    {
+                        results[i, j, k] = array[i + j + k];
+                    }
+                }
+            }
+            return results;
+        }
+
+        public static T[,,,] Reshape<T>(this IEnumerable<T> enumerable, (int, int, int, int) ints)
+        {
+            var array = enumerable as T[] ?? enumerable.ToArray();
+            var results = new T[ints.Item1, ints.Item2, ints.Item3, ints.Item4];
+            for (var i = 0; i < ints.Item1; i++)
+            {
+                for (var j = 0; j < ints.Item2; j++)
+                {
+                    for (var k = 0; k < ints.Item3; k++)
+                    {
+                        for (var l = 0; l < ints.Item4; l++)
+                        {
+                            results[i, j, k, l] = array[i + j + k + l];
+                        }
+                    }
+                }
+            }
+            return results;
+        }
     }
+}
+
 }
