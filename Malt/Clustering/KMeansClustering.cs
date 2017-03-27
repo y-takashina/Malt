dda943fb24f84c3f9e0e8773a539f614771e141b
ytakashina <takashina2051@gmail.com>
2017-03-28 00:57:10 +0900

    Fixed Clustering.KMeansClustering

--- Malt/Clustering/KMeansClustering.cs
+++ Malt/Clustering/KMeansClustering.cs
@@ -1,46 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MoreLinq;
 
 namespace Malt.Clustering
 {
     public static partial class Clustering
     {
-        public static Tuple<double[], int[]> KMeansClustering(double[] data, int n)
+        public static (double[] means, int[] assignments) KMeansClustering(double[] data, int n)
         {
             var rand = new Random();
-            var means = data.OrderBy(v => rand.Next()).Take(n).ToList();
+            var means = data.OrderBy(v => rand.Next()).Take(n).ToArray();
             var assignments = new int[data.Length];
             while (true)
             {
                 var prevAssignments = assignments.Select(v => v).ToArray();
-                assignments = data.Select(v => means.IndexOf(means.MinBy(m => Math.Pow(v - m, 2)))).ToArray();
+                for (var i = 0; i < data.Length; i++)
+                {
+                    var min = double.MaxValue;
+                    var mini = 0;
+                    for (var j = 0; j < means.Length; j++)
+                    {
+                        var d = Math.Pow(data[i] - means[j], 2);
+                        if (d < min)
+                        {
+                            min = d;
+                            mini = j;
+                        }
+                    }
+                    assignments[i] = mini;
+                }
                 if (Enumerable.Range(0, data.Length).All(i => assignments[i] == prevAssignments[i])) break;
-                means = means.Select((m, i) => data.Where((v, j) => i == assignments[j]).DefaultIfEmpty(m).Average()).ToList();
+                means = means.Select((m, i) => data.Where((v, j) => i == assignments[j]).DefaultIfEmpty(m).Average()).ToArray();
             }
-            return Tuple.Create(means.ToArray(), assignments);
+            return (means, assignments);
         }
 
-        public static Tuple<double[], int[]> KMedoidsClustering(double[] data, int n)
+        public static (double[] medoids, int[] assignments) KMedoidsClustering(double[] data, int n)
         {
             var rand = new Random();
-            var medoids = data.OrderBy(v => rand.Next()).Take(n).ToList();
+            var medoids = data.OrderBy(v => rand.Next()).Take(n).ToArray();
             var assignments = new int[data.Length];
             while (true)
             {
                 var prevAssignments = assignments.Select(v => v).ToArray();
-                assignments = data.Select(v => medoids.IndexOf(medoids.MinBy(m => Math.Pow(v - m, 2)))).ToArray();
+                for (var i = 0; i < data.Length; i++)
+                {
+                    var min = double.MaxValue;
+                    var mini = 0;
+                    for (var j = 0; j < medoids.Length; j++)
+                    {
+                        var d = Math.Pow(data[i] - medoids[j], 2);
+                        if (d < min)
+                        {
+                            min = d;
+                            mini = j;
+                        }
+                    }
+                    assignments[i] = mini;
+                }
                 if (Enumerable.Range(0, data.Length).All(i => assignments[i] == prevAssignments[i])) break;
-                for (var i = 0; i < medoids.Count; i++)
+                for (var i = 0; i < medoids.Length; i++)
                 {
                     var members = data.Where((v, j) => i == assignments[j]).ToList();
                     medoids[i] = members.IndexOf(members.Min(m => members.Sum(v => Math.Pow(v - m, 2))));
                 }
             }
-            return Tuple.Create(medoids.ToArray(), assignments);
+            return (medoids, assignments);
         }
     }
 }
