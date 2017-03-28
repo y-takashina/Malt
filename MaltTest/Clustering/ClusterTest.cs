296dfdcc2586ea12112a4d524c6159c8315453b9
ytakashina <takashina2051@gmail.com>
2017-03-28 09:50:01 +0900

    Add ClusterTest

---
+++ MaltTest/Clustering/ClusterTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using Malt.Clustering;
+using static Malt.Clustering.Clustering;
+
+namespace MaltTest.Clustering
+{
+    public class ClusterTest
+    {
+        [Fact]
+        public void PrintTest()
+        {
+            var cluster = Cluster(Cluster(35), Cluster(Cluster(Cluster(Cluster(77), Cluster(Cluster(34), Cluster(22))), Cluster(Cluster(Cluster(69),
+                Cluster(Cluster(69), Cluster(33))), Cluster(Cluster(95), Cluster(22)))), Cluster(Cluster(Cluster(8), Cluster(38)), Cluster(72))));
+            cluster.Print();
+        }
+    }
+}
